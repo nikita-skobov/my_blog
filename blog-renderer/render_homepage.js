114d811873e17908dbff130f890cf6b35fdc781d
nikita-skobov <nskobov.websites@gmail.com>
2020-04-03 20:22:39 -0500

    adds proper html output to render homepage script

--- blog-renderer/render_homepage.js
+++ blog-renderer/render_homepage.js
@@ -4,16 +4,27 @@ import fs from 'fs'
 
 const args = process.argv.slice(2)
 
-console.log(args)
-console.log('\n')
+const blogHome = 'Nikita\'s blog'
+const blogHomeURL = 'https://blog.nikitas.link'
+const name = 'Nikita Skobov'
+const nameURL = 'https://nikitas.link'
+const description = 'My name is Nikita Skobov and this is my blog. I am a student of statistics, and computer science at the University of Minnesota. Sometimes I write code for fun.'
+
+const rightNow = new Date()
 
 const blogs = []
 
 for (let i = 0; i < args.length; i += 1) {
     if (i % 2 == 0) {
         // the even ones are the titles:
+        const titleWithoutHashtag = args[i].substr(2)
+        let blogFileName = titleWithoutHashtag.toLowerCase()
+        blogFileName = blogFileName.replace(/\s+/g, '-')
+        blogFileName = blogFileName.replace(/\'+/g, '')
+        blogFileName = blogFileName.replace(/,+/g, '')
         blogs.push({
-            title: args[i]
+            title: titleWithoutHashtag,
+            path: blogFileName,
         })
     } else {
         // the odd ones are the tags
@@ -21,11 +32,88 @@ for (let i = 0; i < args.length; i += 1) {
     }
 }
 
-blogs.forEach((blog) => {
-    console.log(`blog name: ${blog.title}`)
-    console.log('tags:')
-    console.log(blog.tags)
+renderBlogPostLink = (blogObj) => {
+    return `<a href="${blogHomeURL}/${blogObj.path}">${blogObj.title}</a>`
+}
+let blogListHTMLString = ''
+blogs.forEach((blogObj) => {
+    blogListHTMLString = `${blogListHTMLString}${renderBlogPostLink(blogObj)}\n    `
 })
+
+const myhtml = `<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>${blogHome}</title>
+    <meta name="HandheldFriendly" content="True">
+    <link rel="shortcut icon" href="/favicon.png" type="image/png">
+    <link rel="canonical" href="${blogHomeURL}/">
+    <meta property="og:site_name" content="${blogHome}">
+    <meta property="og:title" content="${blogHome}">
+    <meta property="og:description" content="${description}">
+    <meta property="og:url" content="${blogHomeURL}/">
+    <meta property="article:published_time" content="${rightNow.toISOString()}">
+    <meta property="article:modified_time" content="${rightNow.toISOString()}">
+    <meta property="article:publisher" content="${nameURL}">
+    <style>
+        h1 {
+            font-size: 3.2em !important;
+            font-weight: 600 !important;
+            letter-spacing:  -1px !important;
+            margin-bottom: 0% !important;
+        }
+        h2, h3, h4, h5, h6 {
+            font-size: 2.5em !important;
+        }
+        p, li, input {
+            font-weight: 300 !important;
+            line-height: 2.1 !important;
+            font-size: 20px !important;
+        }
+
+        .navbar {
+            min-height: 60px !important;
+            background-color: black;
+            position: relative;
+        }
+        html, body {
+            margin: 0;
+        }
+        .blog-title {
+            color: white;
+            font-size: 36px;
+            margin-bottom: 0;
+            border-bottom: none;
+            text-decoration: none;
+        }
+        .wrapper {
+            padding-left: 30px;
+            padding-right: 30px;
+            position: absolute;
+            width: 95%;
+            top: 50%;
+            margin: auto;
+            text-align: center;
+            transform: translate(0, -50%);
+        }
+    </style>
+</head>
+<body>
+    <header class="navbar">
+        <div class="wrapper">
+            <a class="blog-title" href="${blogHomeURL}">${blogHome}</a>
+        </div>
+    </header>
+    <div>
+    ${blogListHTMLString}
+    </div>
+</body>
+</html>`
+
+console.log(myhtml)
+
 
 // const absolutePathToFile = args[0]
 // if (!absolutePathToFile) {
